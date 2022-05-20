240dd8a05adc57f22a7a4d6e665254469021282a
Roman Ratushnyi <remenok9@gmail.com>
2022-05-20 13:49:03 +0300

    Added checkbox

---
+++ src/ToDo/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+const ToDo = (props) => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todoText) => {
+    const newTodo = {
+      text: todoText,
+      isDone: false,
+      id: Date.now(),
+    };
+
+    setTodos([...todos, newTodo]);
+  };
+
+  const removeTodo = (id) => {
+    // filter
+  };
+
+  const toggleTodoCompletion = (id) => {
+    // map
+  };
+
+  const onSubmit = (values, formikBag) => {
+    // todo
+    addTodo(values.text);
+    formikBag.resetForm();
+  };
+  return (
+    <article>
+      <div>
+        <Formik initialValues={{ text: "" }} onSubmit={onSubmit}>
+          <Form>
+            <Field name="text" placeholder="todo text" />
+            <button type="submit">Add task</button>
+          </Form>
+        </Formik>
+      </div>
+      <div>
+        <h2>Tasks:</h2>
+        <ul>
+          {todos.map((todo) => (
+            <li>
+              <input
+                type="checkbox"
+                name="isDone"
+                id={todo.id}
+                checked={todo.isDone}
+                disabled
+              />
+              {todo.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </article>
+  );
+};
+
+export default ToDo;
