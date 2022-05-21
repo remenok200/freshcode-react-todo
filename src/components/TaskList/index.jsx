14109312b906d186b604bdb0f98dc6851ddb4ed1
Roman Ratushnyi <remenok9@gmail.com>
2022-05-21 16:19:24 +0300

    Add CompletedTodos component

--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ACTION_TYPES from "../ToDo/reducer/actionTypes";
+import CompletedTodos from "./CompletedTodos";
 
 const TaskList = (props) => {
   const [editID, setEditID] = useState(null);
@@ -34,48 +35,14 @@ const TaskList = (props) => {
         ) : (
           <table>
             <tbody>
-              {state.todos.map((todo) => (
-                <tr key={todo.id}>
-                  <td>
-                    <input
-                      onChange={() => toggleTodoCompletion(todo.id)}
-                      type="checkbox"
-                      name="isDone"
-                      id={todo.id}
-                      checked={todo.isDone}
-                    />
-                  </td>
-                  <td>
-                    {editID === todo.id ? (
-                      <input
-                        onChange={editTodoHandler}
-                        type="text"
-                        placeholder={todo.text}
-                      />
-                    ) : (
-                      <span>{todo.text}</span>
-                    )}
-                  </td>
-                  <td>
-                    {editID === todo.id ? (
-                      <button onClick={() => editTodo(todo.id)}>
-                        Save task!
-                      </button>
-                    ) : (
-                      <button onClick={() => setEditID(todo.id)}>
-                        Edit task
-                      </button>
-                    )}
-                  </td>
-                  <td>
-                    {todo.isDone && (
-                      <button onClick={() => removeTodo(todo.id)}>
-                        Remove task
-                      </button>
-                    )}
-                  </td>
-                </tr>
-              ))}
+              <CompletedTodos 
+              editTodo={editTodo}
+              setEditID={setEditID}
+              toggleTodoCompletion={toggleTodoCompletion}
+              editID={editID}
+              editTodoHandler={editTodoHandler}
+              removeTodo={removeTodo}
+              state={state} />
             </tbody>
           </table>
         )}
